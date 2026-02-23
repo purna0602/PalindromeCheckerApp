c5c7c73e5de4147723a64eb76c11e18d866ff13f
ada-lo <adarshsr221073@gmail.com>
2026-02-23 15:47:10 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeApp/src/PalindromeApp.java
+++
@@ -1,7 +0,0 @@
-public class PalindromeApp
-{
-    static void main(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-    }
-}
